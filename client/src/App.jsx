0bc4302c416feb881e1525d1772a2f483d501686
bytesbybrave <bahadurali123470@gmail.com>
2026-03-30 17:33:58 +0500

    feat: restructure App component to render Home page and remove unused Homepage file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
-import Navbar from './components/navbar.jsx'
-import Hero from './components/Hero.jsx'
-import Process from './components/Process.jsx'
-import WhyChooseUs from './components/WhyChooseUs.jsx'
+import Home from './pages/Home.jsx'
 
 const App = () => {
   return (
-    <main className="min-h-screen relative bg-background overflow-hidden flex flex-col">
-      <Navbar />
-      <Hero />
-      <Process />
-      <WhyChooseUs />
-    </main>
+    <Home />
   )
 }
 
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Navbar from '../components/navbar.jsx'
+import Hero from '../components/Hero.jsx'
+import Process from '../components/Process.jsx'
+import WhyChooseUs from '../components/WhyChooseUs.jsx'
+
+const Home = () => {
+  return (
+    <main className="min-h-screen relative bg-background overflow-hidden flex flex-col">
+      <Navbar />
+      <Hero />
+      <Process />
+      <WhyChooseUs />
+    </main>
+  )
+}
+
+export default Home
